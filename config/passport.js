6cc18e1f44150ff4aa2932e629c9749ddfe3b870
varshajangid07 <07varshajangid@gmail.com>
2026-08-14 13:04:37 +0530

    Update GoogleStrategy callbackURL to use environment variable

--- config/passport.js
+++ config/passport.js
@@ -27,7 +27,7 @@ passport.use(new LocalStrategy({ usernameField : 'email' }, async(email, passwor
 passport.use(new GoogleStrategy({
     clientID : process.env.GOOGLE_CLIENT_ID,
     clientSecret : process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL : "http://localhost:2000/auth/google/callback"
+    callbackURL : process.env.GOOGLE_CALLBACK_URL || "http://localhost:2000/auth/google/callback"
 },
     async(accessToken, refreshToken, profile, done)=>{
         try{
